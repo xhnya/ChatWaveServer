dd4717835cc3df92b880bfe6ca953c05f4226757
肖海南 <hain.work@outlook.com>
2024-10-25 10:13:00 +0800

    初始化项目

---
+++ src/main/java/com/xhn/chat/chatwaveserver/ChatWaveServerApplication.java
@@ -0,0 +1,13 @@
+package com.xhn.chat.chatwaveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWaveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWaveServerApplication.class, args);
+	}
+
+}
